82665dbe1d8d04f19019f760b2cedbad0d24a904
nadeemsahu <nadeemsahun@gmail.com>
2026-06-12 12:20:38 +0530

    refactor: rename LibraryHCE to LibraryCard and drop LibraryReader module

--- LibraryReader/src/main/java/com/piotrekwitkowski/libraryreader/DESFireReaderException.java
+++
@@ -1,7 +0,0 @@
-package com.piotrekwitkowski.libraryreader;
-
-class DESFireReaderException extends Exception {
-    DESFireReaderException(String message) {
-        super(message);
-    }
-}
--- LibraryReader/src/main/java/com/piotrekwitkowski/libraryreader/StudentIdException.java
+++
@@ -1,7 +0,0 @@
-package com.piotrekwitkowski.libraryreader;
-
-class StudentIdException extends Exception {
-    StudentIdException(String message) {
-        super(message);
-    }
-}
